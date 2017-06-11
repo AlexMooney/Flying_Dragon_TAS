86cbbfd66a3b2f6b8ebfd0ca06fe411fcb911cf4
Alex Mooney <AlexMMooney@gmail.com>
2017-06-10 20:44:35 -0400

    Start of elimination round lua stuff

--- Flying_Dragon_-_The_Secret_Scroll_(U).lua
+++ Flying_Dragon_-_The_Secret_Scroll_(U).lua
@@ -1,5 +1,5 @@
 textY = 96
-textHeight = 12
+textHeight = 14
 
 enemy1type = 0x0741
 enemy1X = 0x0743
@@ -52,116 +52,137 @@ end
 while true do
 	c = 0
 
-	gui.text (68, 39, "HP: " .. memory.readbyte(playerHitPoints) .. "  [" .. math.max(0, memory.readbyte(playerIFrames)*8 - memory.readbyte(globalClock)%8) .. "]", "white")
-	gui.text (58, 55, "EHP: " .. memory.readbyte(enemyHitPoints) .. "  [" .. memory.readbyte(bossIFrames) .. "]", "white")
-
 	pX = memory.readbyte (0x0703)
-	SX = 0x0100*memory.readbyte(0x0017) + memory.readbyte (0x0016)
-	pXfrac = memory.readbyte (0x009D)
-	pY = memory.readbyte (0x0702)
-	xVel = memory.readbyte (0x00C8)
-	inv = memory.readbyte (0x00E9)
-	bossInv = memory.readbyte (0x048D)
-	pCool = memory.readbyte(0x070E)
-	enemies = memory.readbyte(0x048A)
-	spawn = memory.readbyte(0x00EB)*8 - memory.readbyte(globalClock)%8
-	--glov = memory.readbyte (0x0428)
-	--boots = memory.readbyte (0x0429)
-	--rng = memory.readbyte (0x00DA)
-	--pause = memory.readbyte (0x1A)
+	if (pX > 0) then
+		gui.text (68, 39, "HP: " .. memory.readbyte(playerHitPoints) .. "  [" .. math.max(0, memory.readbyte(playerIFrames)*8 - memory.readbyte(globalClock)%8) .. "]", "white")
+		gui.text (58, 55, "EHP: " .. memory.readbyte(enemyHitPoints) .. "  [" .. memory.readbyte(bossIFrames) .. "]", "white")
 
-	--pausestr = "";
-	--if pause % 64 == 0 then
-		--pausestr = "UNPAUSE!!!"
-	--elseif pause % 64 == 62 then
-		--pausestr = "PAUSE!!!"
-	--else
-		--pausestr = "" .. pause % 64 .. "(" .. pause .. ")"
-	--end
+		SX = 0x0100*memory.readbyte(0x0017) + memory.readbyte (0x0016)
+		pXfrac = memory.readbyte (0x009D)
+		pY = memory.readbyte (0x0702)
+		xVel = memory.readbyte (0x00C8)
+		inv = memory.readbyte (0x00E9)
+		bossInv = memory.readbyte (0x048D)
+		pCool = memory.readbyte(0x070E)
+		enemies = memory.readbyte(0x048A)
+		spawn = memory.readbyte(0x00EB)*8 - memory.readbyte(globalClock)%8
+		--glov = memory.readbyte (0x0428)
+		--boots = memory.readbyte (0x0429)
+		--rng = memory.readbyte (0x00DA)
+		--pause = memory.readbyte (0x1A)
 
-	--rngVal = bizstring.binary (memory.readbyte (0x8000 + rng, "System Bus"))
+		--pausestr = "";
+		--if pause % 64 == 0 then
+			--pausestr = "UNPAUSE!!!"
+		--elseif pause % 64 == 62 then
+			--pausestr = "PAUSE!!!"
+		--else
+			--pausestr = "" .. pause % 64 .. "(" .. pause .. ")"
+		--end
 
-	printText (string.format ("SX: %3d X: %3d [%3d] Y: %3d Vx: %2d cnt: %2d sp: %2d e: %2d", SX, pX, pXfrac, pY, xVel, pCool, spawn, enemies))
+		--rngVal = bizstring.binary (memory.readbyte (0x8000 + rng, "System Bus"))
 
-	if (last["X"]) then
-		printText (string.format ("SX: %3d X: %3d [%3d] Y: %3d Vx: %2d", last["SX"], last["X"], last["Xfrac"], last["Y"], last["xVel"]), "gray")
-	end
+		printText (string.format ("SX: %3d X: %3d [%3d] Y: %3d Vx: %2d cool: %2d kills: %1d", SX, pX, pXfrac, pY, xVel, pCool, enemies))
 
-	--beh = memory.readbyte (playerBeh)
-	--printText (string.format ("Behaviours: Atk: %d Ldr: %d Jmp: %d Item: %d", bit.band(beh, 128), bit.band(beh, 8), bit.band(beh, 1), memory.readbyte(itemCounter)))
-	--printText (string.format ("Ointment: %d, Boots: %d, Frame Counter: %s", oint, boots, pausestr))
-
-	--rock = memory.readbyte(0xD7)
-	--if rock > 0 then
-		--printText (string.format ("Rock: %d", rock))
-	--end
-
-	playerHeight = 32
-	playerWidth = 16
-	animation = memory.readbyte(0x0704)
-	if (animation == 12) then
-		playerHeight = 16
-	elseif (animation == 8) then
-		playerWidth = 36
-	elseif (animation == 128 or animation == 129) then
-		playerWidth = 36
-	end
-
-	gui.drawRectangle (memory.readbyte(0x703) + deltaWidth, memory.readbyte(0x702) + deltaHeight + (32 - playerHeight), playerWidth, playerHeight, "white")
-
-	c = c + 1
-
-  --printText (string.format ("%2s %3s %10s %3s %3s %3s %3s %4s", "E", "Type", "(  X,   Y)", "P1", "P2", "P3", "Inv", "MInv"))
-	printText (string.format ("%2s %4s %10s %3s %3s", "E", "Type", "(  X,   Y)", "P1", "P2"))
-	--printText (string.format ("%2s %4s %10s %3s %3s", "E", "Type", "(  X,   Y)", "P1", "P2"), nil, nil, 300)
-
-	for i = 0, numSprites - 1, 1 do
-		type = memory.readbyte (enemy1type + 0x10*i)
-		if type > 0 then
-
-			phasedur = memory.readbyte (enemy1phaseduration + 0x10*i)
-			phasecount = memory.readbyte (enemy1phasecounter + 0x10*i)
-			--phasetimer = memory.readbyte (enemy1phasetimer + 0x10*i)
-			--minv = memory.readbyte (enemy1hitbymagiccounter + 0x10*i)
-			--inv = memory.readbyte  (enemy1hitbyswordcounter + 0x10*i)
-			eX = memory.readbyte(enemy1X + 0x10*i)
-			eY = memory.readbyte(enemy1Y + 0x10*i)
-
-			--spriteX = memory.readbyte (spritesXstart + i)
-			--spriteY = memory.readbyte (spritesYstart + i)
-			--sprite = memory.readbyte (spritesLocation + i)
-			--spriteSize = memory.readbyte (spriteSizes + sprite, "System Bus")
-			--spriteWidth = memory.readbyte (spriteWidths + spriteSize, "System Bus")
-			--spriteHeight = memory.readbyte (spriteHeights + spriteSize, "System Bus")
-
-			printText (string.format ("%2d %4d (%3d, %3d) %3d %3d", i + 1, type, eX, eY, phasedur, phasecount))
-
-			-- Draw hitboxes around enemies
-			gui.drawText (eX + deltaWidth+12, eY + deltaHeight, i+1, "white", "black", 10)
-			gui.drawRectangle(eX + deltaWidth + 7, eY + deltaHeight, 4, 32, "blue")
+		if (last["X"]) then
+			printText (string.format ("SX: %3d X: %3d [%3d] Y: %3d Vx: %2d", last["SX"], last["X"], last["Xfrac"], last["Y"], last["xVel"]), "gray")
 		end
-	end
 
-	-- Draw useful hitboxes (for cosmic saucers)
-	for i = 1, 3, 1 do
-		if memory.readbyte(0x0701 + 0x10*i) > 0 then
-			hX = memory.readbyte(0x0703 + 0x10*i)
-			hY = memory.readbyte(0x0702 + 0x10*i)
+		--beh = memory.readbyte (playerBeh)
+		--printText (string.format ("Behaviours: Atk: %d Ldr: %d Jmp: %d Item: %d", bit.band(beh, 128), bit.band(beh, 8), bit.band(beh, 1), memory.readbyte(itemCounter)))
+		--printText (string.format ("Ointment: %d, Boots: %d, Frame Counter: %s", oint, boots, pausestr))
 
-			hW = 16
-			hH = 1
-			gui.drawRectangle (hX + deltaWidth, hY + deltaHeight + 16 + 7, hW, hH, "white")
+		--rock = memory.readbyte(0xD7)
+		--if rock > 0 then
+			--printText (string.format ("Rock: %d", rock))
+		--end
+
+		playerHeight = 32
+		playerWidth = 16
+		animation = memory.readbyte(0x0704)
+		if (animation == 12) then
+			playerHeight = 16
+		elseif (animation == 8) then
+			playerWidth = 36
+		elseif (animation == 128 or animation == 129) then
+			playerWidth = 36
 		end
+
+		gui.drawRectangle (memory.readbyte(0x703) + deltaWidth, memory.readbyte(0x702) + deltaHeight + (32 - playerHeight), playerWidth, playerHeight, "white")
+
+		c = c + 1
+
+	  --printText (string.format ("%2s %3s %10s %3s %3s %3s %3s %4s", "E", "Type", "(  X,   Y)", "P1", "P2", "P3", "Inv", "MInv"))
+		printText (string.format ("%2s %4s %10s %3s %3s %6s: %2d", "E", "Type", "(  X,   Y)", "P1", "P2", "Spawn", spawn))
+		--printText (string.format ("%2s %4s %10s %3s %3s", "E", "Type", "(  X,   Y)", "P1", "P2"), nil, nil, 300)
+
+		for i = 0, numSprites - 1, 1 do
+			type = memory.readbyte (enemy1type + 0x10*i)
+			if type > 0 then
+
+				phasedur = memory.readbyte (enemy1phaseduration + 0x10*i)
+				phasecount = memory.readbyte (enemy1phasecounter + 0x10*i)
+				--phasetimer = memory.readbyte (enemy1phasetimer + 0x10*i)
+				--minv = memory.readbyte (enemy1hitbymagiccounter + 0x10*i)
+				--inv = memory.readbyte  (enemy1hitbyswordcounter + 0x10*i)
+				eX = memory.readbyte(enemy1X + 0x10*i)
+				eY = memory.readbyte(enemy1Y + 0x10*i)
+
+				--spriteX = memory.readbyte (spritesXstart + i)
+				--spriteY = memory.readbyte (spritesYstart + i)
+				--sprite = memory.readbyte (spritesLocation + i)
+				--spriteSize = memory.readbyte (spriteSizes + sprite, "System Bus")
+				--spriteWidth = memory.readbyte (spriteWidths + spriteSize, "System Bus")
+				--spriteHeight = memory.readbyte (spriteHeights + spriteSize, "System Bus")
+
+				printText (string.format ("%2d %4d (%3d, %3d) %3d %3d", i + 1, type, eX, eY, phasedur, phasecount))
+
+				-- Draw hitboxes around enemies
+				gui.drawText (eX + deltaWidth+12, eY + deltaHeight, i+1, "white", "black", 10)
+				gui.drawRectangle(eX + deltaWidth + 7, eY + deltaHeight, 4, 32, "blue")
+			end
+		end
+
+		-- Draw useful hitboxes (for cosmic saucers)
+		for i = 1, 3, 1 do
+			if memory.readbyte(0x0701 + 0x10*i) > 0 then
+				hX = memory.readbyte(0x0703 + 0x10*i)
+				hY = memory.readbyte(0x0702 + 0x10*i)
+
+				hW = 16
+				hH = 1
+				gui.drawRectangle (hX + deltaWidth, hY + deltaHeight + 16 + 7, hW, hH, "white")
+			end
+		end
+
+
+
+		last["SX"] = SX
+		last["X"] = pX
+		last["Xfrac"] = pXfrac
+		last["Y"] = pY
+		last["xVel"] = xVel
+		--last["RNG"] = rng
+		--last["RNGVal"] = rngVal
+	else -- tournament fight
+		gui.text(68, 39, "HP: " .. memory.readbyte(playerHitPoints), "white")
+		gui.text(58, 55, "EHP: " .. memory.readbyte(enemyHitPoints), "white")
+
+		pY = memory.readbyte( 0x0682)
+		pX = memory.readbyte( 0x0683)
+		eY = memory.readbyte( 0x06C2)
+		eX = memory.readbyte( 0x06C3)
+		potion = memory.readbyte( 0x0780)
+		potionX = memory.readbyte( 0x0782)
+		potionY = memory.readbyte( 0x0783)
+
+		atkCnt = memory.readbyte( 0x06E7)
+
+		printText(string.format("You   X: %3d Y: %3d", pX, pY))
+		printText(string.format("Enemy X: %3d Y: %3d", eX, eY))
+
+		c = c+1
+		printText(string.format("Attack Cnt: %2d", atkCnt))
 	end
-
-
 	emu.frameadvance();
-
-	last["SX"] = SX
-	last["X"] = pX
-	last["Xfrac"] = pXfrac
-	last["Y"] = pY
-	last["xVel"] = xVel
-	--last["RNG"] = rng
-	--last["RNGVal"] = rngVal
 end
